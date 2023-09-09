5a24a008fb5853a6b66a3fca2fa17a7d2065d968
Lyndsey Dickson <lyndsey.dickson@l3harris.com>
2023-09-09 15:07:23 -0400

    Initialize repo. Run a lua script.

---
+++ src/main.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const ziglua = @import("ziglua");
+
+pub fn main() !void {
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    var stdout = std.io.getStdOut().writer();
+
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    // Initialize Lua vm
+    var lua = try ziglua.Lua.init(allocator);
+    defer lua.deinit();
+
+    // Open the Lua standard libraries
+    lua.openLibs();
+
+    lua.doString("print('Hello from Lua!')") catch unreachable;
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
