97ab4e7119e1c612ede2db7d3436f765898220b0
Lyndsey Dickson <lyndsey.dickson@l3harris.com>
2023-09-13 23:28:25 -0400

    create a script engine struct to evaluate lua code

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,62 @@ const yaml = @cImport({
     @cInclude("libfyaml.h");
 });
 
+const Allocator = std.mem.Allocator;
+
+const ScriptEngine = struct {
+    allocator: Allocator,
+    lua: ziglua.Lua,
+
+    pub fn eval(self: *ScriptEngine, str: []const u8) !void {
+        // Form a function
+        var lua_function = std.ArrayList(u8).init(self.allocator);
+        defer lua_function.deinit();
+        var function_writer = lua_function.writer();
+        try function_writer.print("function eval_script()\n", .{});
+        var lines = std.mem.split(u8, str, "\n");
+        while (lines.next()) |line| {
+            try function_writer.print("    {s}\n", .{line});
+        }
+        try function_writer.print("end\n", .{});
+        try lua_function.append(0);
+
+        // Compile the function
+        try self.lua.doString(@ptrCast(lua_function.items));
+        const stack_size = self.lua.getTop();
+        const lua_type = try self.lua.getGlobal("eval_script");
+        if (lua_type == ziglua.LuaType.function) {
+            try self.lua.protectedCall(0, ziglua.mult_return, 0);
+
+            // Get each of the returned values
+            const num_returns = self.lua.getTop() - stack_size;
+            var stdout = std.io.getStdOut().writer();
+            try stdout.print("Number of returned values {d}\n",
+                .{ num_returns });
+            for (0..@intCast(num_returns)) |_| {
+                if (self.lua.isInteger(-1)) {
+                    const result = try self.lua.toInteger(-1);
+                    try stdout.print("Lua returned {d}\n", .{ result });
+                    self.lua.pop(1);
+                }
+            }
+        }
+    }
+
+    pub fn init(allocator: Allocator) !ScriptEngine {
+        // Initialize Lua vm
+        var lua = try ziglua.Lua.init(allocator);
+        lua.openLibs();
+        return ScriptEngine{
+            .allocator = allocator,
+            .lua = lua,
+        };
+    }
+
+    pub fn deinit(self: *ScriptEngine) void {
+        self.lua.deinit();
+    }
+};
+
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
@@ -17,37 +73,11 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    // Initialize Lua vm
-    var lua = try ziglua.Lua.init(allocator);
-    defer lua.deinit();
-
-    // Open the Lua standard libraries
-    lua.openLibs();
-
     // Run some lua code
-    const lua_code =
-        \\function eval_script()
-        \\    return 42, 2
-        \\end
-    ;
-    lua.doString(lua_code) catch unreachable;
-    const stack_size = lua.getTop();
-    const lua_type = try lua.getGlobal("eval_script");
-    if (lua_type == ziglua.LuaType.function) {
-        try lua.protectedCall(0, ziglua.mult_return, 0);
-
-        // Get each of the returned values
-        const num_returns = lua.getTop() - stack_size;
-        try stdout.print("Number of returned values {d}\n",
-            .{ num_returns });
-        for (0..@intCast(num_returns)) |_| {
-            if (lua.isInteger(-1)) {
-                const result = try lua.toInteger(-1);
-                try stdout.print("Lua returned {d}\n", .{ result });
-                lua.pop(1);
-            }
-        }
-    }
+    const lua_code = "return 42, 2";
+    var script_engine = try ScriptEngine.init(allocator);
+    defer script_engine.deinit();
+    try script_engine.eval(lua_code);
 
     try stdout.print("Yaml version {s}\n", .{yaml.fy_library_version()});
 }
